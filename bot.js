629534fb39cf7bb1af6638d5fe877b24c04fdfc0
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-03-21 15:40:05 +0200

    add feaute to send tiktok videos directly to TG

--- bot.js
+++ bot.js
@@ -5,10 +5,12 @@ const Telegraf = require("telegraf");
 const Extra = require("telegraf/extra");
 const Markup = require("telegraf/markup");
 const session = require("telegraf/session");
+const TikTokScraper = require("tiktok-scraper-ts");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+const urlRegex =
+  /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
 
 bot.start((ctx) => ctx.reply("start"));
 
@@ -18,13 +20,23 @@ bot.catch((err) => {
 });
 
 bot.hears(urlRegex, async (ctx) => {
-    const link  = ctx.match[0]
-    if(link.includes('tiktok.com')){
-        ctx.reply('tiktok link')
-        return;
+  const link = ctx.match[0];
+  if (link.includes("tiktok.com")) {
+    try {
+      const videoData = await TikTokScraper.fetchVideo(link, true);
+      const videoLink = videoData.downloadURL;
+      console.l;
+      ctx.replyWithVideo(videoLink, {
+        reply_to_message_id: ctx.update.message.message_id,
+      });
+    } catch (err) {
+      console.log(err);
+      ctx.reply("Some error");
     }
-    ctx.reply('got a link (not a tiktok)')
-})
+    return;
+  }
+  ctx.reply("got a link (not a tiktok)");
+});
 
 bot.launch();
 
