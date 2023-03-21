da7ca37b7930a4d3bbf2107b44c5cbcb78292b94
Ivan Labiak <71697480+ILabiak@users.noreply.github.com>
2023-03-21 15:16:46 +0200

    add action when link is passed to bot

--- bot.js
+++ bot.js
@@ -7,13 +7,24 @@ const Markup = require("telegraf/markup");
 const session = require("telegraf/session");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
-bot.start(async (ctx) => ctx.reply("start"));
 
+const urlRegex = /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/;
+
+bot.start((ctx) => ctx.reply("start"));
 
 bot.use(session());
 bot.catch((err) => {
   console.log(err);
 });
+
+bot.hears(urlRegex, async (ctx) => {
+    const link  = ctx.match[0]
+    if(link.includes('tiktok.com')){
+        ctx.reply('tiktok link')
+        return;
+    }
+    ctx.reply('got a link (not a tiktok)')
+})
 
 bot.launch();
 
